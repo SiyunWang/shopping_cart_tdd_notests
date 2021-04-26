f51d3138ddc3e21ff65c7f798c42d52c5af48e3d
Siyun Wang <siyun@Siyuns-MacBook-Pro.local>
2021-04-26 10:24:09 -0700

    Create Cart class

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,15 @@
 package com.galvanize.cart;
 
+import java.util.ArrayList;
+
 public class Cart {
+    private ArrayList<Item> cart = new ArrayList<Item>();
+
+    public Cart() {
+
+    }
+
+    public ArrayList<Item> getCart() {
+        return cart;
+    }
 }
--- src/test/java/com/galvanize/cart/CartTest.java
+++ src/test/java/com/galvanize/cart/CartTest.java
@@ -10,5 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CartTest {
 
+    @Test
+    public void cartIsEmptyForNewUser() {
+        //setup
+        Cart testCart = new Cart();
+        int expected = 0;
+        //execute
+        int actual = testCart.getCart().size();
+        //assert
+        assertEquals(expected, actual);
+    }
+
 
 }
