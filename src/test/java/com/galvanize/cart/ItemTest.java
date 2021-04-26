7f368c6780330ad10638f5d0fdd1e42d5dd78635
Elizabeth Hayt <enhayt@gmail.com>
2021-04-26 12:53:01 -0700

    Add ItemTest and toggleSaleMethod test

---
+++ src/test/java/com/galvanize/cart/ItemTest.java
@@ -0,0 +1,25 @@
+package com.galvanize.cart;
+
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ItemTest {
+  @Test
+  public void toggleSaleMethod() {
+    //setup
+    Item testItem = new Item("apple", 1.99);
+    boolean expected = true;
+    //execute
+    testItem.toggleSale();
+    boolean actual = testItem.getOnSale();
+    //assert
+    assertEquals(expected, actual);
+  }
+}
